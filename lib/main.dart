cf4a1713f4c1848f5471b51094f96a9890c4adc5
unknown <hmiol5577@gmail.com>
2024-09-30 10:21:06 +0300

    delete unnecessary file and update main.dart file accordingly

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,10 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/home_page.dart';
-import 'firebase_options.dart';
+//import 'firebase_options.dart';
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
-await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-);
- FirebaseFirestore.instance.settings = const Settings(
-    persistenceEnabled: true,
-  );
+await Firebase.initializeApp();
   runApp(
     MaterialApp(
      home: MyApp()
